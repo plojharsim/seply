4e94407a57207bf352672f482534aac6e989ca9c
Šimon Plojhar <plojharsim@gmail.com>
2024-12-14 11:53:57 +0100

    Updated

--- script.js
+++ script.js
@@ -1,4 +1,48 @@
 function toggleMenu() {
     const menu = document.getElementById('nav-list');
-    menu.classList.toggle('show'); // Přepne třídu "show", čímž se menu zobrazí/skryje
+    menu.classList.toggle('show');
 }
+
+document.getElementById("reservationForm").addEventListener("submit", function(e) {
+    e.preventDefault();
+
+    if (this.brand.value === "Značka nebyla vybrána") {
+        document.getElementById("responseMessage").textContent = "Vyberte značku kotle!";
+        document.getElementById("responseMessage").style.color = "red";
+        return false;  // Zabrání odeslání formuláře
+    }
+
+    const fullname = document.getElementById("fullname").value;
+    const email = document.getElementById("email").value;
+    const phone = document.getElementById("phone").value;
+    const brand = document.getElementById("brand").value;
+    const time = document.getElementById("time").value;
+    const message = document.getElementById("message").value;
+
+    const formData = {
+        fullname: fullname,
+        email: email,
+        phone: phone,
+        brand: brand,
+        time: time,
+        message: message
+    };
+
+    const scriptURL = 'https://script.google.com/macros/s/AKfycbxVopxS5hQpjtoHvubiKmzEwRQ5zOGHRLPQRREghyIYhpg1sN5AW6YDXGoJTA1ekM_O/exec';  // Nahraďte vaší vlastní URL
+
+    fetch(scriptURL, {
+        method: 'POST',
+        body: new URLSearchParams(formData),
+    })
+    .then(response => response.text())
+    .then(result => {
+        document.getElementById("responseMessage").textContent = "Formulář byl úspěšně odeslán!";
+        document.getElementById("responseMessage").style.color = "white";
+        document.getElementById("reservationForm").reset();
+    })
+    .catch(error => {
+        document.getElementById("responseMessage").textContent = "Chyba při odesílání formuláře. Zkuste to znovu.";
+        document.getElementById("responseMessage").style.color = "red";
+        console.error("Error:", error);
+    });
+});

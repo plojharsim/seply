7e4d8edb8e590fc345b72c3e6bc6b4977b4f4bbe
plojharsim <plojharsim@gmail.com>
2026-04-12 19:22:13 +0200

    Remove unnecessary blank line in script.js

--- script.js
+++ script.js
@@ -2,5 +2,4 @@ function toggleMenu() {
     const menu = document.getElementById('nav-list');
     menu.classList.toggle('show');
 }
-
 document.getElementById("year").textContent = new Date().getFullYear();
